0292baa8901c765ddbc70419dd8289e939e40fb9
dawidy10 <davidburaga10@gmail.com>
2025-07-09 17:30:25 +0300

    Added avatar and info to hero section

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body>
-				<div className="w-full px-20 py-20">{children}</div>
+				<div className="w-full px-20 py-10">{children}</div>
 			</body>
 		</html>
 	);
--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,8 +1,24 @@
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { MapPin } from "lucide-react";
 export default function HeroSection() {
 	return (
-		<div className="w-full h-[80vh] relative">
-			<Image alt="HeroBG" fill objectFit="cover" src="/home-bg.jpg" />
+		<div className="w-full h-[80vh] relative bg-foreground rounded-md overflow-hidden">
+			<Image alt="HeroBG" fill objectFit="cover" src="/home-bg.jpg" className="opacity-40" />
+			<div className="absolute z-10 left-10 bottom-10 flex items-center gap-4">
+				<Avatar className="w-40 h-40">
+					<AvatarImage src="avatar.jpg" />
+					<AvatarFallback className="bg-foreground text-white">DB</AvatarFallback>
+				</Avatar>
+				<div className="flex flex-col gap-1 text-white">
+					<h1 className="text-5xl">David Buraga</h1>
+					<p className="text-xl">Developer & Designer</p>
+					<div className="flex gap-1">
+						<MapPin fill="white" size={20} />
+						<p>Iași, Romania</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }

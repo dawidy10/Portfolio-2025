6aa7acf54ab9e11217200aea25fcd6f26d418fff
dawidy10 <davidburaga10@gmail.com>
2025-07-10 14:12:26 +0300

    About section

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,13 @@ export default function Home() {
 	return (
 		<>
 			<HeroSection />
+			<div className="w-full border-l-1 border-r-1 mt-10 px-14 py-10">
+				<h1 className="text-5xl">Hi, I'm David.</h1>
+				<p className="mt-4 text-lg w-1/2">
+					I’m a computer engineering student who likes to code cool projects and make them look good.
+				</p>
+				<h1 className="text-5xl mt-15">Projects</h1>
+			</div>
 		</>
 	);
 }

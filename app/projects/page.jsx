053e53ac255258a1e45c382089daab01a1a152b9
dawidy10 <davidburaga10@gmail.com>
2026-03-10 13:46:44 +0200

    Added intentional to projects page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,30 +1,35 @@
-import ProjectCard from "@/components/ProjectCard";
+import ProjectCard from '@/components/ProjectCard'
 export default function Projects() {
-	return (
-		<>
-			<h1 className="text-5xl mt-20">Personal & Client Work</h1>
-			<div className="grid gird-cols-1 lg:grid-cols-3 gap-x-8 gap-y-14 w-full mt-20">
-				<ProjectCard
-					image="/BlindHopeLogoBlack.svg"
-					tags={["Game Development", "Logo Design"]}
-					link="blind-hope"
-				/>
-				<ProjectCard
-					image="/ApologiaLogoBlack.svg"
-					tags={["Branding", "Logo Design"]}
-					link="apologia-reformata"
-				/>
-				<ProjectCard image="/SharkLogoBlack.svg" tags={["Logo Design", "Branding"]} link="shark" />
+  return (
+    <>
+      <h1 className="text-5xl mt-20">Personal & Client Work</h1>
+      <div className="grid gird-cols-1 lg:grid-cols-3 gap-x-8 gap-y-14 w-full mt-20">
+        <ProjectCard
+          image="/BlindHopeLogoBlack.svg"
+          tags={['Game Development', 'Logo Design']}
+          link="blind-hope"
+        />
+        <ProjectCard
+          image="/IntentionalLogoBlack.svg"
+          tags={['Web Development', 'PWA']}
+          link="intentional"
+        />
+        <ProjectCard
+          image="/ApologiaLogoBlack.svg"
+          tags={['Branding', 'Logo Design']}
+          link="apologia-reformata"
+        />
+        <ProjectCard image="/SharkLogoBlack.svg" tags={['Logo Design', 'Branding']} link="shark" />
 
-				<ProjectCard
-					image="/transformatLogoBlack.svg"
-					tags={["Logo Design", "Branding", "Clothing Design"]}
-					link="transformat"
-				/>
+        <ProjectCard
+          image="/transformatLogoBlack.svg"
+          tags={['Logo Design', 'Branding', 'Clothing Design']}
+          link="transformat"
+        />
 
-				<ProjectCard image="/IancuLogoBlack.svg" tags={["Logo Design", "Branding"]} link="iancu" />
-			</div>
-			<p className="mt-30 text-center font-bold">davidburaga 2025</p>
-		</>
-	);
+        <ProjectCard image="/IancuLogoBlack.svg" tags={['Logo Design', 'Branding']} link="iancu" />
+      </div>
+      <p className="mt-30 text-center font-bold">davidburaga 2025</p>
+    </>
+  )
 }
